490b04f6e978daaad7249d901f136abcb06c5e08
yemrealtanay <y.emrealtanay@gmail.com>
2021-10-08 17:52:45 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".typing", {
+
+    strings: ["Full Stack Developer", "Youtuber", "Writer", "3D Designer", "Game Designer", "Php Laravel Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+
+});
+
